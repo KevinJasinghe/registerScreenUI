1cc82b3946a4af2a7cfb02a5c2c11062211b4bcc
KevinJasinghe <kevinjasinghe@gmail.com>
2022-02-07 21:17:30 -0500

    s

---
+++ registerScreen/SignUpButton.swift
@@ -0,0 +1,18 @@
+//
+//  SignUpButton.swift
+//  registerScreen
+//
+//  Created by Kevin Jasinghe on 2022-02-07.
+//
+
+import UIKit
+
+class SignUpButton: UIButton {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.backgroundColor = UIColor(red: 111/255, green: 160/255, blue: 250/255, alpha: 1).cgColor
+    }
+
+}
